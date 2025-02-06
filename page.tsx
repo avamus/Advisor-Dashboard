e310484b901a37b9b0e46bf1bc69d9155cb56c06
avamus <lukassys2004@gmail.com>
2025-02-06 23:59:10 +0100

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import AdvisorDashboard from "@/components/ui/advisor-dashboard"
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-white">
+      <AdvisorDashboard />
+    </main>
+  )
+}
